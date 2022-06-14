6d3269f0bb2742da604203a0d28a87cb74623fdd
bluelaned <43365136+bluelaned@users.noreply.github.com>
2022-06-14 23:45:27 +0700

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,12 +13,13 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        'guru',
         'novel',
         '/edit-novel/{id}',
         '/update-novel/{id}',
         '/delete-novel/{id}',
         
-        'guru',
+        
         '/edit-guru/{id}',
         '/update-guru/{id}',
         '/delete-guru/{id}'
