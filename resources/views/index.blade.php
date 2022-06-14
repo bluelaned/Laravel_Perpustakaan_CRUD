37f3ed0d4bd9cee64d65edb7bfd654fb4619e542
bluelaned <43365136+bluelaned@users.noreply.github.com>
2022-06-15 00:28:42 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
 
 <head> 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 <!-- Primary Meta Tags -->
 <title>Volt - Free Bootstrap 5 Dashboard</title>
 
