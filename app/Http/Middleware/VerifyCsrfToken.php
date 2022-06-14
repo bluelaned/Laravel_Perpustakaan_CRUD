0ff8f144e32a343d3b504c8fa47e093d2b06b654
bluelaned <43365136+bluelaned@users.noreply.github.com>
2022-06-14 23:51:27 +0700

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -17,5 +17,10 @@ class VerifyCsrfToken extends Middleware
         '/edit-novel/{id}',
         '/update-novel/{id}',
         '/delete-novel/{id}',
+
+        'guru',
+        '/edit-guru/{id}',
+        '/update-guru/{id}',
+        '/delete-guru/{id}'
     ];
 }
